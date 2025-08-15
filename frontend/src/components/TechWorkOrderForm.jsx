c8b6fa9dd47a2d13763c6bfff0299e0e45a5458d
Landon Perzee <landon@greatlakeslifting.com>
2025-08-15 10:14:19 -0500

    fixed tech dashboard

--- frontend/src/components/TechWorkOrderForm.jsx
+++ frontend/src/components/TechWorkOrderForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import API from '../api';
