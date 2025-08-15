12f3d35afc559914fb1c0ddd27dbc0298ce62fab
Landon Perzee <landon@greatlakeslifting.com>
2025-08-15 14:06:35 -0500

    added offline functionality, roll back to previous deploy if broken

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import offlineAPI from './services/offlineAPI';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000',
-  //baseURL: process.env.REACT_APP_API_URL || 'https://glls-work-order-portal.onrender.com',
+  //baseURL: 'http://localhost:4000',
+  baseURL: process.env.REACT_APP_API_URL || 'https://glls-work-order-portal.onrender.com',
   timeout: 10000,
 });
 
