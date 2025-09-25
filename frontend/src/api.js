ff990438443eade30a91d5188d3644731c67167e
Landon Perzee <landon@greatlakeslifting.com>
2025-09-25 16:04:48 -0500

    base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: 'http://localhost:4000',
-  //baseURL: process.env.REACT_APP_API_URL || 'https://glls-work-order-portal.onrender.com',
+  //baseURL: 'http://localhost:4000',
+  baseURL: process.env.REACT_APP_API_URL || 'https://glls-work-order-portal.onrender.com',
 });
 export default API;
